50bd142b0ca694a7aa5a94beecabc743772ac817
Jakub Havelka <jakub.havelka@atos.net>
2020-06-08 21:42:56 +0200

    Initial commit for Notes-React-app

---
+++ Notes-React-app/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NotesIcon from '@material-ui/icons/Notes';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <NotesIcon /> Sticky Notes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
